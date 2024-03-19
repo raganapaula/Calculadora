a96da52149068052f14116b11d776cc6fb908b1f
Ana Paula Ragievicz <aragievicz@gmail.com>
2024-03-19 15:17:59 -0300

    Fechamento da View L68

--- Calculadora/App.js
+++ Calculadora/App.js
@@ -58,7 +58,6 @@ export default function App() {
     setCurrentNumber(currentNumber + buttonPressed)
   }
 
-
   return (
     <View style={styles.container}>
 
@@ -66,7 +65,7 @@ export default function App() {
       <View style={styles.results}>
         <Text style={styles.historyText}>{lastNumber}</Text>
         <Text style={styles.resultText}>{currentNumber}</Text>
-      <View>
+      <View/>
 
       {/* Area onde os botões são exibidos*/}
       <View style={styles.buttons}>
